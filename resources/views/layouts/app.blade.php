39d8cee9dc32e120fd732dafb08efb11f6490f97
Tammy Robinson <tammyprodev@gmail.com>
2022-09-16 13:51:27 -0500

    Replace controller with full page livewire component
    # Conflicts:
    #	resources/views/layouts/app.blade.php
    #	resources/views/layouts/navigation.blade.php
    #	resources/views/welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <livewire:styles />
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -19,7 +20,7 @@
 
             <!-- Page Heading -->
             <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
             </header>
@@ -29,5 +30,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <livewire:scripts />
     </body>
 </html>
