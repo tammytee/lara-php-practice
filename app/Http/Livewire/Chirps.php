0bad9094b0532dbb0e3645f906cadf6f04c55eac
Tammy Robinson <tammyprodev@gmail.com>
2022-09-16 15:27:05 -0500

    Save chirps on form submission and display

--- app/Http/Livewire/Chirps.php
+++ app/Http/Livewire/Chirps.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Chirp;
 use Livewire\Component;
 
 class Chirps extends Component
 {
+    public Chirp $chirp;
+
+    protected $rules = [
+        'chirp.message' => 'required|string',
+    ];
+
+    public function __construct()
+    {
+        $this->chirp = new Chirp();
+    }
+
+    public function saveChirp()
+    {
+        $this->validate();
+
+        auth()->user()->chirps()->save($this->chirp);
+
+        $this->chirp->save();
+
+        $this->reset();
+    }
+
     public function render()
     {
-        return view('livewire.chirps')
-            ->layout('layouts.app', ['header' => 'Chirps'])
-            ->section('slot');
+        return view('livewire.chirps', [
+            'chirps' => Chirp::all()->sortByDesc('created_at'),
+        ])
+        ->layout('layouts.app', ['header' => 'Chirps'])
+        ->section('slot');
     }
 }
